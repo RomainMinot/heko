1d18d8077ff761a5c66e95773833b698d65081eb
Romainminot <rom_ino@hotmail.fr>
2022-06-12 19:41:09 +0200

    Hero

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,9 @@
+//Styles
 import 'normalize.css'
 import '../style/style.scss';
+//Images
+import logo from '../img/logo.png'
+import heroImg from '../img/hero.jpg'
 
-console.log('Hello world');
+document.querySelector('.header__logo').src = logo;
+document.querySelector('.hero__img').src = heroImg;
